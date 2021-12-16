000cd572f99c89bfe14990ed7511172e4c3f77df
MarcinMatuk <marcin.mates@gmail.com>
2021-12-16 14:35:27 +0100

    app

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Marcin Matuk on 10/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
